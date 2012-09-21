d48736f81c56a4a98a0c1dc743293f248bba29bb
Stefan heimes <stefan_heimes@hotmail.com>
2012-09-21 18:51:13 +0200

    Update language files

---
+++ src/system/modules/metamodelsattribute_translatedselect/languages/en/tl_metamodel_attribute.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   MetaModels
+ * @subpackage Backend
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  CyberSpectrum
+ * @license    private
+ * @filesource
+ */
+if (!defined('TL_ROOT'))
+{
+	die('You cannot access this file directly!');
+}
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedselect']    = 'Translated select';
+
+?>
