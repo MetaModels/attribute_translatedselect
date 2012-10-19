8fb94655297536cc3b7c0c5fd1441d3dcf90f903
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-10-20 00:17:03 +0200

    use new setting "all_langs" also for translatedselect.

---
+++ src/system/modules/metamodelsattribute_translatedselect/dca/tl_metamodel_filtersetting.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   MetaModels
+ * @subpackage Backend
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  CyberSpectrum
+ * @license    private
+ * @filesource
+ */
+if (!defined('TL_ROOT'))
+{
+	die('You cannot access this file directly!');
+}
+
+/**
+ * Table tl_metamodel_filtersetting
+ */
+
+$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['simplelookup_palettes']['translatedselect'] = &$GLOBALS['TL_DCA']['tl_metamodel_filtersetting']['simplelookup_palettes']['_translated_'];
+
+?>
