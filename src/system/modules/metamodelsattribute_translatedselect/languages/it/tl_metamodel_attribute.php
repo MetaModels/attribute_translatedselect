b109efb3e30ce285d50609989b9501b0383a36bb
Mini Model <minimodel@metamodel.me>
2013-06-27 20:14:39 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_translatedselect/languages/it/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_translatedselect/languages/it/tl_metamodel_attribute.php
@@ -8,8 +8,10 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-04T02:35:28+02:00
+ * last-updated: 2013-06-23T04:14:34+02:00
  */
 
-
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['0'] = 'Colonna di ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['1'] = 'Specificare quale colonna della tabella non tradotta deve essere utilizzata per l\'ordinamento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['0']   = 'Tabella non tradotta per l\'ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['1']   = 'Specificare la tabella che mostra la colonna di ordinamento.';
