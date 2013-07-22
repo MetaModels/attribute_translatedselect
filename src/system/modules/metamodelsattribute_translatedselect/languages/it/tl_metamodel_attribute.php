c366db0142f946a176f7846c96db11a809ade6a1
Mini Model <minimodel@metamodel.me>
2013-07-22 12:44:37 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_translatedselect/languages/it/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_translatedselect/languages/it/tl_metamodel_attribute.php
@@ -8,10 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-06-23T04:14:34+02:00
+ * last-updated: 2013-07-10T10:06:10+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['0'] = 'Colonna di ordinamento';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['1'] = 'Specificare quale colonna della tabella non tradotta deve essere utilizzata per l\'ordinamento.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['0']   = 'Tabella non tradotta per l\'ordinamento';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['1']   = 'Specificare la tabella che mostra la colonna di ordinamento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['0']          = 'Colonna di ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['1']          = 'Specificare quale colonna della tabella non tradotta deve essere utilizzata per l\'ordinamento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['0']            = 'Tabella non tradotta per l\'ordinamento';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['1']            = 'Specificare la tabella che mostra la colonna di ordinamento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedselect'] = 'Select della traduzione';
