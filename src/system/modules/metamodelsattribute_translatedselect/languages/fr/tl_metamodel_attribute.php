170199cfcb19ef18efbe61fae56009b965cce32d
Mini Model <minimodel@metamodel.me>
2013-09-30 08:14:44 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_translatedselect/languages/fr/tl_metamodel_attribute.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-04T02:35:28+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['0']          = 'colonne de classement';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['1']          = 'Veuillez spécifier quelle colonne de la table non convertie doit être utilisée pour le classement.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['0']            = 'Table non convertie pour classement';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['1']            = 'Veuillez spécifier la table qui fournit la colonne de classement';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedselect'] = 'Sélection convertie';
