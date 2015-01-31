9013eead237f552aff83fb7e3754c5aff94d1caf
Stefan Heimes <stefan_heimes@hotmail.com>
2015-01-31 17:05:56 +0100

    Drop sortable and flag.

--- contao/dca/tl_metamodel_dcasetting.php
+++ contao/dca/tl_metamodel_dcasetting.php
@@ -31,8 +31,6 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id'
     ),
     'overview'     => array(
         'filterable',
-        'searchable',
-        'sortable',
-        'flag'
+        'searchable'
     )
 );
