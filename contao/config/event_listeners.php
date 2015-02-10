0e1e36f3107c3a49e3553832358265690a4816e4
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-02-10 05:17:51 +0100

    Drop config.php and add event_listeners.php.

---
+++ contao/config/event_listeners.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package     MetaModels
+ * @subpackage  AttributeTranslatedSelect
+ * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright   The MetaModels team.
+ * @license     LGPL.
+ * @filesource
+ */
+
+use MetaModels\Attribute\TranslatedSelect\AttributeTypeFactory;
+use MetaModels\Attribute\Events\CreateAttributeFactoryEvent;
+use MetaModels\DcGeneral\Events\Table\Attribute\TranslatedSelect\Subscriber;
+use MetaModels\Events\MetaModelsBootEvent;
+use MetaModels\MetaModelsEvents;
+
+return array
+(
+    MetaModelsEvents::SUBSYSTEM_BOOT_BACKEND => array(
+        function (MetaModelsBootEvent $event) {
+            new Subscriber($event->getServiceContainer());
+        }
+    ),
+    MetaModelsEvents::ATTRIBUTE_FACTORY_CREATE => array(
+        function (CreateAttributeFactoryEvent $event) {
+            $factory = $event->getFactory();
+            $factory->addTypeFactory(new AttributeTypeFactory());
+        }
+    ),
+);
