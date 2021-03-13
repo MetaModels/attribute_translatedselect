e8d9e43012cc0a763448a40206a05341a85f33b9
Mini Model <minimodel@metamodel.me>
2021-03-13 16:38:40 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/de/tl_metamodel_attribute.php
@@ -1,21 +1,31 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_translatedselect.
+ *
+ * (c) 2012-2021 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2021-03-13T16:38:36+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2018-04-08T21:12:31+02:00
+ * @copyright 2012-2021 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_translatedselect/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
+
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_langcolumn']['0']          = 'Sprachenspalte';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_langcolumn']['1']          = 'Wählen Sie bitte die Sprachenspalte.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['0']          = 'Sortierspalte';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srcsorting']['1']          = 'Wählen Sie bitte, welche Spalte der noch nicht übersetzten Tabelle für die Sortierung verwendet werden soll.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['0']            = 'Nicht übersetzte Tabelle für die Sortierung.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['1']            = 'Bitte nennen Sie die Tabelle welche die zu sortieren de Spalte enthält.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_srctable']['1']            = 'Bitte nennen Sie die Tabelle, welche die zu sortierende Spalte enthält.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['translatedselect'] = 'Übersetzte Einzelauswahl [select]';
 
