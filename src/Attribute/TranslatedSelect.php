af3475ae738fee6b8a26125cb0140ca10b78d5d9
Ingolf Steinhardt <info@e-spin.de>
2022-08-12 12:17:22 +0200

    Rebuild sql queries to add fallback

--- src/Attribute/TranslatedSelect.php
+++ src/Attribute/TranslatedSelect.php
@@ -27,6 +27,7 @@
 
 namespace MetaModels\AttributeTranslatedSelectBundle\Attribute;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\FetchMode;
 use MetaModels\Attribute\ITranslated;
 use MetaModels\AttributeSelectBundle\Attribute\Select;
@@ -138,34 +139,33 @@ class TranslatedSelect extends Select implements ITranslated
             return null;
         }
 
-        $strColNameWhere = $this->getAdditionalWhere();
-        $strColNameAlias = $this->getAliasColumn();
-        if (!$strColNameAlias) {
-            $strColNameAlias = $this->getIdColumn();
-        }
+        $tableName   = $this->getSelectSource();
+        $idColumn    = $this->getIdColumn();
+        $aliasColumn = $this->getAliasColumn();
+        $valueColumn = $this->getValueColumn();
+        $langColumn  = $this->getLanguageColumn();
+        $sortColumn  = $this->getSortingColumn();
 
-        // Easy out, we have the correct language.
-        if ($varValue[$this->getLanguageColumn()] == $this->getMetaModel()->getActiveLanguage()) {
-            return $varValue[$strColNameAlias];
-        }
-
-        // Translate to current language.
         $builder = $this->connection->createQueryBuilder()
-            ->select('s.*')
-            ->from($this->getSelectSource(), 's')
-            ->where($this->getIdColumn() . '=:id')
-            ->andWhere($this->getLanguageColumn() . '=:language')
-            ->setParameter('id', $varValue[$this->getIdColumn()])
-            ->setParameter('language', $this->getMetaModel()->getActiveLanguage());
+            ->select(\sprintf('IFNULL (j.%1$s, t.%1$s) as %1$s', $aliasColumn))
+            ->from($tableName, 't')
+            ->leftJoin(
+                't',
+                $tableName,
+                'j',
+                \sprintf('t.%1$s = j.%1$s AND j.%2$s = :activeLanguage', $idColumn, $langColumn)
+            )
+            ->setParameter('activeLanguage', $this->getMetaModel()->getActiveLanguage())
+            ->where('t.' . $langColumn . ' = :fallbackLanguage')
+            ->where('t.' . $idColumn . '=:id')
+            ->setParameter('fallbackLanguage', $this->getMetaModel()->getFallbackLanguage())
+            ->setParameter('id', $varValue[$this->getIdColumn()]);
 
-        if ($strColNameWhere) {
-            $builder->andWhere($strColNameWhere);
+        if (false === ($result = $builder->executeQuery()->fetchAssociative())) {
+            return null;
         }
 
-        $statement = $builder->executeQuery();
-        $result    = $statement->fetchAssociative();
-
-        return $result[$strColNameAlias];
+        return $result[$aliasColumn];
     }
 
     /**
@@ -173,6 +173,10 @@ class TranslatedSelect extends Select implements ITranslated
      */
     public function widgetToValue($varValue, $itemId)
     {
+        if (null === $varValue) {
+            return null;
+        }
+
         $strColNameAlias = $this->getAliasColumn();
         $strColNameId    = $this->getIdColumn();
         $strColNameWhere = $this->getAdditionalWhere();
@@ -183,23 +187,27 @@ class TranslatedSelect extends Select implements ITranslated
         }
 
         $builder = $this->connection->createQueryBuilder()
-            ->select('*')
-            ->from($this->getSelectSource())
-            ->where($strColNameAlias . '=:alias')
-            ->andWhere($strColNameLang . ' IN (:languages)')
+            ->select('t.*')
+            ->from($this->getSelectSource(), 't')
+            ->where('t.' . $strColNameAlias . '=:alias')
+            ->andWhere('t.' . $strColNameLang . ' IN (:languages)')
             ->setParameter('alias', $varValue)
             ->setParameter(
                 'languages',
-                [$this->getMetaModel()->getActiveLanguage(), $this->getMetaModel()->getFallbackLanguage()]
+                [$this->getMetaModel()->getActiveLanguage(), $this->getMetaModel()->getFallbackLanguage()],
+                Connection::PARAM_STR_ARRAY
             );
 
         if ($strColNameWhere) {
             $builder->andWhere($strColNameWhere);
         }
 
-        return $builder
-            ->executeQuery()
-            ->fetchAssociative();
+        if (false === ($result = $builder->executeQuery()->fetchAssociative())) {
+
+            return null;
+        }
+
+        return $result;
     }
 
     /**
@@ -228,74 +236,48 @@ class TranslatedSelect extends Select implements ITranslated
     {
         $tableName   = $this->getSelectSource();
         $idColumn    = $this->getIdColumn();
+        $aliasColumn = $this->getAliasColumn();
+        $valueColumn = $this->getValueColumn();
         $langColumn  = $this->getLanguageColumn();
         $addWhere    = $this->getAdditionalWhere();
-        $firstOrder  = $this->getFilterOptionsOrderBy();
-        $secondOrder = $this->getSortingColumn();
-        $langSet     = \sprintf(
-            '\'%s\',\'%s\'',
-            $this->getMetaModel()->getActiveLanguage(),
-            $this->getMetaModel()->getFallbackLanguage()
-        );
+        $sortColumn  = $this->getSortingColumn();
 
-        if ($usedOnly) {
-            $statement = $this
-                ->connection
-                ->prepare(
-                    \sprintf(
-                        'SELECT COUNT(%1$s.%2$s) as mm_count, %1$s.*
-                            FROM %3$s
-                            LEFT JOIN %1$s ON (%1$s.id = (SELECT
-                                %1$s.id
-                                FROM %1$s
-                                WHERE %5$s IN (%6$s)
-                                AND (%1$s.%2$s=%3$s.%4$s)
-                                %7$s
-                                ORDER BY FIELD(%1$s.%5$s,%6$s)
-                                LIMIT 1
-                            ))
-                            GROUP BY %1$s.%2$s
-                            ORDER BY %9$s %8$s',
-                        // @codingStandardsIgnoreStart - we want to keep the numbers at the end of the lines below.
-                        $tableName,                                // 1
-                        $idColumn,                                 // 2
-                        $this->getMetaModel()->getTableName(),     // 3
-                        $this->getColName(),                       // 4
-                        $langColumn,                               // 5
-                        $langSet,                                  // 6
-                        ($addWhere ? ' AND ('.$addWhere.')' : ''), // 7
-                        $secondOrder,                              // 8
-                        $firstOrder                                // 9
-                        // @codingStandardsIgnoreEnd
-                    )
-                );
-            $result = $statement->executeQuery();
+        $builder = $this->connection->createQueryBuilder();
+        $builder
+            ->select(
+                \sprintf(
+                    'COUNT(t.%1$s) as mm_count, IFNULL (j.%1$s, t.%1$s) as %1$s, IFNULL (j.%2$s, t.%2$s) as %2$s',
+                    $aliasColumn,
+                    $valueColumn
+                )
+            )
+            ->from($tableName, 't')
+            ->leftJoin(
+                't',
+                $tableName,
+                'j',
+                \sprintf('t.%1$s = j.%1$s AND j.%2$s = :activeLanguage', $idColumn, $langColumn)
+            )
+            ->setParameter('activeLanguage', $this->getMetaModel()->getActiveLanguage())
+            ->where('t.' . $langColumn . ' = :fallbackLanguage')
+            ->setParameter('fallbackLanguage', $this->getMetaModel()->getFallbackLanguage())
+            ->groupBy('t.' . $idColumn)
+            ->orderBy('t.' . $sortColumn);
 
-            return $result;
+        if ($addWhere) {
+            $builder->andWhere($addWhere);
         }
 
-        $statement = $this
-            ->connection
-            ->prepare(
-                \sprintf(
-                    'SELECT COUNT(%1$s.%2$s) as mm_count, %1$s.*
-                        FROM %1$s
-                        WHERE %3$s IN (%4$s)
-                        %5$s
-                        GROUP BY %1$s.%2$s
-                        ORDER BY %7$s %6$s',
-                    // @codingStandardsIgnoreStart - we want to keep the numbers at the end of the lines below.
-                    $tableName,                                // 1
-                    $idColumn,                                 // 2
-                    $langColumn,                               // 3
-                    $langSet,                                  // 4
-                    ($addWhere ? ' AND ('.$addWhere.')' : ''), // 5
-                    $secondOrder,                              // 6
-                    $firstOrder                                // 7
-                    // @codingStandardsIgnoreEnd
-                )
-            );
-        $result = $statement->executeQuery();
+        if ($usedOnly) {
+            $subSelect = $this->connection->createQueryBuilder();
+            $subSelect
+                ->select('m.' . $this->getColName())
+                ->from($this->getMetaModel()->getTableName(), 'm');
+
+            $builder->andWhere($builder->expr()->in('t.' . $idColumn, $subSelect->getSQL()));
+        }
+
+        $result = $builder->executeQuery();
 
         return $result;
     }
@@ -349,21 +331,21 @@ class TranslatedSelect extends Select implements ITranslated
                         $strColNameId,                                               // 2
                         $this->getMetaModel()->getTableName(),                       // 3
                         $this->getColName(),                                         // 4
-                        \implode(',', $idList),                                       // 5
+                        \implode(',', $idList),                              // 5
                         ($strColNameWhere ? ' AND (' . $strColNameWhere . ')' : ''), // 6
                         $this->getLanguageColumn(),                                  // 7
                         $strLangSet,                                                 // 8
                         $this->getSortingColumn(),                                   // 9
                         $this->getFilterOptionsOrderBy()                             // 10
-                        // @codingStandardsIgnoreEnd
+                    // @codingStandardsIgnoreEnd
                     )
                 );
-            $objValue->executeQuery([$this->get('id')]);
+            $result   = $objValue->executeQuery();
         } else {
-            $objValue = $this->getFilterOptionsForUsedOnly($usedOnly);
+            $result = $this->getFilterOptionsForUsedOnly($usedOnly);
         }
 
-        return $this->convertOptionsList($objValue, $this->getAliasColumn(), $this->getValueColumn(), $arrCount);
+        return $this->convertOptionsList($result, $this->getAliasColumn(), $this->getValueColumn(), $arrCount);
     }
 
     /**
@@ -427,40 +409,52 @@ class TranslatedSelect extends Select implements ITranslated
         $strColValue        = $this->getValueColumn();
         $strColAlias        = $this->getAliasColumn();
         $strColNameWhere    = $this->getAdditionalWhere();
+        $fallbackLanguage   = $this->getMetaModel()->getFallbackLanguage();
         $arrReturn          = [];
 
         if ($strTableNameId && $strColNameId) {
             $strMetaModelTableName   = $this->getMetaModel()->getTableName();
-            $strMetaModelTableNameId = $strMetaModelTableName.'_id';
+            $strMetaModelTableNameId = $strMetaModelTableName . '_id';
 
             $strPattern = \str_replace(['*', '?'], ['%', '_'], $strPattern);
 
+            if (!\in_array($fallbackLanguage, $arrLanguages)) {
+                $arrLanguages[] = $this->getMetaModel()->getFallbackLanguage();
+            }
+
             // Using aliased join here to resolve issue #3 for normal select attributes
             // (SQL error for self referencing table).
-            $objValue = $this->connection->prepare(\sprintf(
-                'SELECT sourceTable.*, %2$s.id AS %3$s
+            $objValue = $this->connection->prepare(
+                \sprintf(
+                    'SELECT sourceTable.*, %2$s.id AS %3$s
                 FROM %1$s sourceTable
                 RIGHT JOIN %2$s ON (sourceTable.%4$s=%2$s.%5$s)
-                WHERE '.($arrLanguages ? '(sourceTable.%6$s IN (%7$s))' : '').'
+                WHERE ' . ($arrLanguages ? '(sourceTable.%6$s IN (%7$s))' : '') . '
                 AND (sourceTable.%8$s LIKE ? OR sourceTable.%9$s LIKE ?) %10$s',
-                // @codingStandardsIgnoreStart - we want to keep the numbers at the end of the lines below.
-                $strTableNameId,                                      // 1
-                $strMetaModelTableName,                               // 2
-                $strMetaModelTableNameId,                             // 3
-                $strColNameId,                                        // 4
-                $this->getColName(),                                  // 5
-                $strColNameLangCode,                                  // 6
-                '\'' . \implode('\',\'', $arrLanguages) . '\'',        // 7
-                $strColValue,                                         // 8
-                $strColAlias,                                         // 9
-                ($strColNameWhere ? ('AND ' . $strColNameWhere) : '') // 10
-            // @codingStandardsIgnoreEnd
-            ));
+                    // @codingStandardsIgnoreStart - we want to keep the numbers at the end of the lines below.
+                    $strTableNameId,                                 // 1
+                    $strMetaModelTableName,                                 // 2
+                    $strMetaModelTableNameId,                               // 3
+                    $strColNameId,                                          // 4
+                    $this->getColName(),                                    // 5
+                    $strColNameLangCode,                                    // 6
+                    '\'' . \implode('\',\'', $arrLanguages) . '\'', // 7
+                    $strColValue,                                           // 8
+                    $strColAlias,                                           // 9
+                    ($strColNameWhere ? ('AND ' . $strColNameWhere) : '')   // 10
+                // @codingStandardsIgnoreEnd
+                )
+            );
 
-            while ($value = $objValue->executeQuery([$strPattern, $strPattern])->fetchAssociative()) {
+            if (false === ($result = $objValue->executeQuery([$strPattern, $strPattern])->fetchAllAssociative())) {
+                return $arrReturn;
+            }
+
+            foreach ($result as $value) {
                 $arrReturn[] = $value[$strMetaModelTableNameId];
             }
         }
+
         return $arrReturn;
     }
 
@@ -481,7 +475,7 @@ class TranslatedSelect extends Select implements ITranslated
             );
 
             foreach ($arrValues as $intItemId => $arrValue) {
-                $this->connection->prepare($strQuery)->execute([$arrValue[$strColNameId], $intItemId]);
+                $this->connection->prepare($strQuery)->executeQuery([$arrValue[$strColNameId] ?? null, $intItemId]);
             }
         }
     }
@@ -499,32 +493,39 @@ class TranslatedSelect extends Select implements ITranslated
 
         if ($strTableNameId && $strColNameId) {
             $strMetaModelTableName   = $this->getMetaModel()->getTableName();
-            $strMetaModelTableNameId = $strMetaModelTableName.'_id';
+            $strMetaModelTableNameId = $strMetaModelTableName . '_id';
 
             // Using aliased join here to resolve issue #3 for normal select attributes
             // (SQL error for self referencing table).
-            $objValue = $this->connection->prepare(\sprintf(
-                'SELECT sourceTable.*, %2$s.id AS %3$s
+            $objValue = $this->connection->prepare(
+                \sprintf(
+                    'SELECT sourceTable.*, %2$s.id AS %3$s
                 FROM %1$s sourceTable
                 LEFT JOIN %2$s
                     ON ((sourceTable.%7$s=?) AND (sourceTable.%4$s=%2$s.%5$s))
                 WHERE %2$s.id IN (%6$s) %8$s',
-                // @codingStandardsIgnoreStart - we want to keep the numbers at the end of the lines below.
-                $strTableNameId,                                        // 1
-                $strMetaModelTableName,                                 // 2
-                $strMetaModelTableNameId,                               // 3
-                $strColNameId,                                          // 4
-                $this->getColName(),                                    // 5
-                \implode(',', $arrIds),                                  // 6
-                $strColNameLangCode,                                    // 7
-                ($strColNameWhere ? ' AND ('.$strColNameWhere.')' : '') // 8
-            // @codingStandardsIgnoreEnd
-            ));
+                    // @codingStandardsIgnoreStart - we want to keep the numbers at the end of the lines below.
+                    $strTableNameId,                                            // 1
+                    $strMetaModelTableName,                                     // 2
+                    $strMetaModelTableNameId,                                   // 3
+                    $strColNameId,                                              // 4
+                    $this->getColName(),                                        // 5
+                    \implode(',', $arrIds),                             // 6
+                    $strColNameLangCode,                                        // 7
+                    ($strColNameWhere ? ' AND (' . $strColNameWhere . ')' : '') // 8
+                // @codingStandardsIgnoreEnd
+                )
+            );
 
-            while ($value = $objValue->executeQuery([$strLangCode])->fetchAssociative()) {
+            if (false === ($result = $objValue->executeQuery([$strLangCode])->fetchAllAssociative())) {
+                return $arrReturn;
+            }
+
+            foreach ($result as $value) {
                 $arrReturn[$value[$strMetaModelTableNameId]] = $value;
             }
         }
+
         return $arrReturn;
     }
 
